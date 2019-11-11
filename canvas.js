f45384a155b7318e21593ad91bd8d8f33dab825f
Strzempek <martyna.strzempek@atos.net>
2019-11-11 15:52:16 +0100

    Added left-right movement of single color circle

--- canvas.js
+++ canvas.js
@@ -4,34 +4,49 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-c.fillStyle = 'deeppink';
-c.fillRect(50 ,100, 100, 100);
-c.fillStyle = 'blue';
-c.fillRect(400 ,100, 100, 100);
-c.fillStyle = 'green';
-c.fillRect(300 ,300, 100, 100);
-
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 10);
-c.lineTo(400, 300);
-c.strokeStyle = 'pink';
-c.stroke();
+// c.fillStyle = 'deeppink';
+// c.fillRect(50 ,100, 100, 100);
+// c.fillStyle = 'blue';
+// c.fillRect(400 ,100, 100, 100);
+// c.fillStyle = 'green';
+// c.fillRect(300 ,300, 100, 100);
+//
+// // Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 10);
+// c.lineTo(400, 300);
+// c.strokeStyle = 'pink';
+// c.stroke();
 
 // Arc / Circle
-c.beginPath();
-c.arc(200, 250, 50, 0, Math.PI * 2, false);
-c.stroke();
 
-for (let i = 0; i < 100; i++) {
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
+// for (let i = 0; i < 100; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+//   c.beginPath();
+//   c.arc(x, y, 50, 0, Math.PI * 2, false);
+//   let r = Math.random() * 255;
+//   let g = Math.random() * 255;
+//   let b = Math.random() * 255;
+//   c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+//   c.stroke();
+// }
+
+let x = 200;
+let dx = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
   c.beginPath();
-  c.arc(x, y, 50, 0, Math.PI * 2, false);
-  let r = Math.random() * 255;
-  let g = Math.random() * 255;
-  let b = Math.random() * 255;
-  c.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+  c.arc(x, 200, 50, 0, Math.PI * 2, false);
+  c.strokeStyle = 'deeppink';
   c.stroke();
+
+  if (x > innerWidth || x === 0) dx = -dx;
+  x += dx;
 }
+
+animate();
