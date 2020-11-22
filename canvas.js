ad41bcd5000ae353a735c6059cc4d3ad6a1484c1
A694392 <martyna.strzempek@atos.net>
2020-11-22 15:43:13 +0100

    add comments to name each lesson in code

--- canvas.js
+++ canvas.js
@@ -4,24 +4,25 @@ const c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// rectangles
 // c.fillStyle = 'deeppink';
 // c.fillRect(50 ,100, 100, 100);
 // c.fillStyle = 'blue';
 // c.fillRect(400 ,100, 100, 100);
 // c.fillStyle = 'green';
 // c.fillRect(300 ,300, 100, 100);
-//
-// // Line
+
+// Line
 // c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 10);
-// c.lineTo(400, 300);
+// c.moveTo(100, 300);
+// c.lineTo(400, 50);
+// c.lineTo(700, 300);
+// c.lineTo(100, 300);
 // c.strokeStyle = 'pink';
 // c.stroke();
 
 // Arc / Circle
-
-// for (let i = 0; i < 100; i++) {
+// for (let i = 0; i < 50; i++) {
 //   let x = Math.random() * window.innerWidth;
 //   let y = Math.random() * window.innerHeight;
 //   c.beginPath();
@@ -33,11 +34,12 @@ canvas.height = window.innerHeight;
 //   c.stroke();
 // }
 
+// animation of the ball which bounces off the walls
 let x = Math.random() * innerWidth;
 let y = Math.random() * innerHeight;
 let dx = (Math.random() - 0.5) * 8;
 let dy = (Math.random() - 0.5) * 8;
-let radius = 60;
+let radius = 50;
 
 function animate() {
   requestAnimationFrame(animate);
@@ -47,6 +49,8 @@ function animate() {
   c.arc(x, y, radius, 0, Math.PI * 2, false);
   c.strokeStyle = 'deeppink';
   c.stroke();
+  c.fillStyle = 'deeppink';
+  c.fill();
 
   if (x + radius > innerWidth || x - radius < 0) dx = -dx;
   if (y + radius > innerHeight || y - radius < 0) dy = -dy;
